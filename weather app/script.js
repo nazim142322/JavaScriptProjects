3b6fb36878f89dd45e6394eccf9a5df29a94ec0b
nazim142322 <nazim142322@gmail.com>
2023-06-22 23:43:32 +0530

    code added in weather app

--- weather app/script.js
+++ weather app/script.js
@@ -5,15 +5,46 @@ const temperature = document.querySelector('.temperature');
 const description = document.querySelector('.description');
 const humidity = document.getElementById('humidity');
 const wind_speed = document.getElementById('wind-speed');
+const location_not_found = document.querySelector('.location-not-found');
+const weather_body = document.querySelector('.weather-body');
 
 async function checkWeather(city){
     const api_key = "4552d0c351cc6a438e1e99093209de83";
     const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
+    console.log(weather_data);
 
-    console.log(weather_data)
+    //for error (wrong location)
+    if(weather_data.cod ===`404`){
+        location_not_found.style.display='flex';
+        weather_body.style.display="none";  
+        console.log("error");       
+        return;              
+    }
+     
 
+    temperature.innerHTML = `${Math.round(weather_data.main.temp-273.15)}°C`;
+    description.innerHTML=`${weather_data.weather[0].description}`;
+    humidity.innerHTML=`${weather_data.main.humidity}%`;
+    wind_speed.innerHTML=`${weather_data.wind.speed}Km/h`;
+
+    switch(weather_data.weather[0].main){
+            case 'Clouds':
+            weather_img.src="/assets/cloud.png";
+            break;
+            case 'Clear':
+            weather_img.src="/assets/clear.png";
+            break;
+            case 'Rain':
+            weather_img.src="/assets/rain.png";
+            break;
+            case 'Mist':
+            weather_img.src="/assets/mist.png";
+            break;
+            case 'Snow':
+            weather_img.src="/assets/snow.png";            
+    }
 }
 searchBtn.addEventListener('click', ()=>{
     // alert("hello");
